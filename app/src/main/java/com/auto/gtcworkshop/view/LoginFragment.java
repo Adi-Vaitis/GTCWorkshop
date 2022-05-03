47f44d7c17807e606e3dcc7bbd3bce6fc85ce13c
felixioan <stan.felixioan@gmail.com>
2022-05-03 03:06:29 +0200

    changing from activities to fragments the former login classes

---
+++ app/src/main/java/com/auto/gtcworkshop/view/LoginFragment.java
@@ -0,0 +1,26 @@
+package com.auto.gtcworkshop.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.auto.gtcworkshop.R;
+
+public class LoginFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
