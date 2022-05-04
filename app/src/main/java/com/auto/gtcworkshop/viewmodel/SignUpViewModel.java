548cde5fb269ee1cdc3014c1bd125afd715fab25
felixioan <stan.felixioan@gmail.com>
2022-05-04 10:26:06 +0200

    update

--- app/src/main/java/com/auto/gtcworkshop/viewmodel/SignUpViewModel.java
+++ app/src/main/java/com/auto/gtcworkshop/viewmodel/SignUpViewModel.java
@@ -10,12 +10,17 @@ public class SignUpViewModel extends AndroidViewModel {
 
     private final AuthentificationRepository userRepository;
 
-    public RegisterViewModel(Application application)
+    public SignUpViewModel(Application application)
     {super(application);
     userRepository = AuthentificationRepository.getInstance(application);
     }
     public void register(String email, String password)
     {userRepository.register(email,password);}
+
+    public void setSignInpressed(Boolean isSignInpressed)
+    {userRepository.setSignInpressed(isSignInpressed);}
+    public void alreadyUser()
+    {}
 
 
 
