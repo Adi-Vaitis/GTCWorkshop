fca80a5705ab3f112a157daf9715e030c4212e20
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2022-05-05 14:51:30 +0200

    working on the login function

--- app/src/main/java/com/auto/gtcworkshop/viewmodel/SignUpViewModel.java
+++ app/src/main/java/com/auto/gtcworkshop/viewmodel/SignUpViewModel.java
@@ -10,13 +10,14 @@ public class SignUpViewModel extends AndroidViewModel {
 
     private final AuthentificationRepository userRepository;
 
-    public RegisterViewModel(Application application)
-    {super(application);
-    userRepository = AuthentificationRepository.getInstance(application);
+    public SignUpViewModel(Application application) {
+        super(application);
+        userRepository = AuthentificationRepository.getInstance();
     }
-    public void register(String email, String password)
-    {userRepository.register(email,password);}
 
+    public void register(String email, String password) {
+        userRepository.register(email, password);
+    }
 
 
 }
