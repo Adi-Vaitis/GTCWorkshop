fb1843240ab1a059cc5b51ee4fb9890bfc8fd1be
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2022-05-11 09:58:29 +0200

    fragment for adding reservation created

--- app/src/main/java/com/auto/gtcworkshop/view/AddReservationFragment.java
+++ app/src/main/java/com/auto/gtcworkshop/view/AddReservationFragment.java
@@ -1,4 +1,46 @@
 package com.auto.gtcworkshop.view;
 
-public class AddReservationFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.auto.gtcworkshop.R;
+import com.auto.gtcworkshop.viewmodel.ReservationViewModel;
+
+public class AddReservationFragment extends Fragment {
+    private ReservationViewModel reservationViewModel;
+
+    TextView addReservationTitle;
+    TextView infoTitle;
+    EditText feelTextView;
+    EditText modelTextView;
+    EditText problemTextView;
+    EditText millageTextView;
+    Button createReservationButton;
+    Button cancelReservationButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_reservation, container, false);
+
+        addReservationTitle = view.findViewById(R.id.addReservationsTitle);
+        infoTitle = view.findViewById(R.id.infoTitle);
+        feelTextView = view.findViewById(R.id.feelTextView);
+        modelTextView = view.findViewById(R.id.modelTextView);
+        problemTextView = view.findViewById(R.id.problemTextView);
+        millageTextView = view.findViewById(R.id.millageTextView);
+        createReservationButton = view.findViewById(R.id.createReservationButton);
+        cancelReservationButton = view.findViewById(R.id.cancelReservationButton);
+
+        return view;
+    }
 }
