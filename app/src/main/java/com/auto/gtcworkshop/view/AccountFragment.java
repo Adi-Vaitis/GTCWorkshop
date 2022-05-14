85fe540049c99b8f3bc083f12a8847470b9a79f6
Adi-Vaitis <adriangabriel344@gmail.com>
2022-05-14 16:19:37 +0200

    The app is done, but because we have more time, we will implement more features

--- app/src/main/java/com/auto/gtcworkshop/view/AccountFragment.java
+++ app/src/main/java/com/auto/gtcworkshop/view/AccountFragment.java
@@ -2,19 +2,46 @@ package com.auto.gtcworkshop.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.auto.gtcworkshop.R;
 
 public class AccountFragment extends Fragment {
+    NavController navController;
+    Button resetPasswordBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    private void initViews(View view){
+        navController = Navigation.findNavController(view);
+        resetPasswordBtn = view.findViewById(R.id.resetpassbtn);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+
+        resetPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "This feature will be implemented in the future", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
