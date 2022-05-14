cc46bf304b1b2d5ab2bfa725d0ffc4052d6cca84
Adi-Vaitis <adriangabriel344@gmail.com>
2022-05-14 17:06:24 +0200

    Final version of the app

--- app/src/main/java/com/auto/gtcworkshop/view/ContactUsFragment.java
+++ app/src/main/java/com/auto/gtcworkshop/view/ContactUsFragment.java
@@ -2,18 +2,21 @@ package com.auto.gtcworkshop.view;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.auto.gtcworkshop.MainActivity;
 import com.auto.gtcworkshop.R;
 
 public class ContactUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_contact_us, container, false);
     }
 }
